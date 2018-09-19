5d1f68f89044bcaec8c4bfe492f3007d233e3ff9
drafear <drafear@outlook.com>
2018-09-19 23:00:29 +0900

    feat: 対応するbetaページへ移動するように

--- src/link-to-beta.js
+++ src/link-to-beta.js
@@ -1,26 +1,51 @@
-const beta_url = () => {
-  const contest_id = $.url().attr('host').match(/^(.*).contest.atcoder.jp$/)[1];
-  return `https://beta.atcoder.jp/contests/${contest_id}`;
+const convertRules = [
+  {from: '/', to: '/'},
+  {from: '/assignments', to: '/tasks'},
+  {from: '/tasks/*', to: '/tasks/$1'},
+  {from: '/submit', to: '/submit'},
+  {from: '/clarifications', to: '/clarifications'},
+  {from: '/clarifications/update/*', to: '/clarifications/update/$1'},
+  {from: '/users/*', to: '/users/$1'},
+  {from: '/standings', to: '/standings'},
+  {from: '/custom_test', to: '/custom_test'},
+  {from: '/submissions/me', to: '/submissions/me'},
+  {from: '/submissions/all', to: '/submissions'},
+  {from: '/clarifications/insert', to: '/clarifications/insert'},
+];
+
+const getBetaUrl = () => {
+  const contestId = location.host.match(/^(.*).contest.atcoder.jp$/)[1];
+  const prefix = `https://beta.atcoder.jp/contests/${contestId}`;
+  for (const rule of convertRules) {
+    const reg = new RegExp('^' + rule.from.replace(/\*/g, '([^\\/]+)') + '\\/?$');
+    if (location.pathname.match(reg)) {
+      const pathName = location.pathname.replace(reg, rule.to);
+      return prefix + pathName;
+    }
+  }
+  return prefix;
 };
 
-const add_beta = ($ul) => {
+const addBetaButton = $ul => {
   try {
-      $ul.prepend(
-          $("<li>").append(
-              $("<a>").prop("href", beta_url()).append(
-                  $("<img>").attr({
-                      src: chrome.extension.getURL('image/beta.png'),
-                      alt: "β"
-                  }).css({ width: "14px", height: "14px", border: 0 })
-              )
+    $ul.prepend(
+      $('<li>').append(
+        $('<a>')
+          .prop('href', getBetaUrl())
+          .append(
+            $('<img>')
+              .attr({
+                src: chrome.extension.getURL('image/beta.png'),
+                alt: 'β',
+              })
+              .css({width: '14px', height: '14px', border: 0})
           )
-      );
-  }
-  catch (e) {
-  }
+      )
+    );
+  } catch (error) {}
 };
 
 $(() => {
-  const $ul = $(".nav-tabs").eq(0);
-  add_beta($ul);
+  const $ul = $('.nav-tabs').eq(0);
+  addBetaButton($ul);
 });
